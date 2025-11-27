3d6563753353b3705f948b83d3695dd1ee2029f1
energeticakhil <aky881922@gmail.com>
2025-11-28 01:00:44 +0530

    Update waqi-proxy.js

--- netlify/functions/waqi-proxy.js
+++ netlify/functions/waqi-proxy.js
@@ -1,19 +1,31 @@
-export default async (req, res) => {
-  const token = process.env.WAQI_TOKEN;
-  const city = req.query.city;
-
-  if (!city) {
-    return res.status(400).json({ error: "City is required" });
-  }
-
+exports.handler = async (event, context) => {
   try {
+    const token = process.env.WAQI_TOKEN;
+    const city = event.queryStringParameters.city;
+
+    if (!city) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "City missing" })
+      };
+    }
+
     const response = await fetch(
       `https://api.waqi.info/feed/${city}/?token=${token}`
     );
 
     const data = await response.json();
-    return res.status(200).json(data);
-  } catch (error) {
-    return res.status(500).json({ error: "API failed", detail: error.message });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Server error", details: err.message })
+    };
   }
 };
